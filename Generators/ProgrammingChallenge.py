504cb3cd03c37398a171cab4fb1927d6334e8708
mqazizada <mqazizada@eou.edu>
2023-05-31 12:30:03 -0700

    Started ProgrammingChallenge.py

--- Generators/ProgrammingChallenge.py
+++ Generators/ProgrammingChallenge.py
@@ -1,8 +1,24 @@
 import random
 
 
+with open("..\challenges.txt", "r") as file:
+    line = file.readline()
+    #print(line)
+    my_str = ''
+    challenge_list = []
+    while line != '':
+        phrase_list = line.replace("\\n", "\n")
+        for i in phrase_list:
+            my_str += i
+        line = file.readline()
+    print(my_str)
+    # challenge_list.append(my_str)
+    # print(challenge_list)
 
-def main():
-    print("hello world")
-
-main()
+# for i in range(24):
+#
+#
+# def main():
+#     print("hello world")
+#
+# main()
